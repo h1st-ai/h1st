4c9d6a0bbe12f69fae672702baf15f4eed4234d6
ctn <ctn@arimo.com>
2020-08-20 09:40:03 -0700

    Reorg tests into test_hello_world.py

--- examples/HelloWorld/tests/test_rule_based_model.py
+++
@@ -1,6 +0,0 @@
-from ..rule_based_model import RuleBasedModel
-
-def test_rule_based_model():
-    m = RuleBasedModel()
-    for value in range(6):
-        print("Prediction for " + str(value) + " is " + str(m.predict({"value": value})))
