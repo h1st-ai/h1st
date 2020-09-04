037be9c3f14dfb35cc092946eb969ef4cc61c0e2
ctn <ctn@arimo.com>
2020-09-03 22:49:01 -0700

    Fixed even/odd parity error
    😜

--- examples/HelloWorld/rule_based_model.py
+++ examples/HelloWorld/rule_based_model.py
@@ -6,6 +6,9 @@ It doesn't need any data or training, and hence has only a predict() function.
 import h1st as h1
 
 class RuleBasedModel(h1.Model):
+    """
+    Simple rule-based model that "predicts" if a given value is an even number
+    """
     def predict(self, data: dict) -> dict:
-        is_odd = (data["value"] % 2 == 0)
-        return is_odd
+        is_even = (data["value"] % 2 == 0)
+        return is_even
