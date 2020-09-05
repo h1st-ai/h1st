34b3d0ab6711b41fa721068c2a04fe8b3fc9fb65
ctn <ctn@arimo.com>
2020-09-05 00:48:48 -0700

    Trimming

--- h1st/core/trust/explainer.py
+++
@@ -1,7 +0,0 @@
-class Explainer:
-    """
-    An Explainer is one that stores all relevant details about the explanation
-    of a decision that was made by the Model or Graph. For example, details such as
-    `what was the original decision`, `what were the key factors that led to that decision` and
-    `what type of model was used to make the decision` etc.
-    """
