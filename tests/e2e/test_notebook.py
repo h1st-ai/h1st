6600289cb81b931ecd15c52604ad0d3fc3cbf310
Trinh Hai-Anh <anh.hai.trinh@gmail.com>
2020-09-17 22:31:41 -0700

    revert to subprocess.check_call([ipython, AutoCyberTutorial.ipynb], cwd=nb_folder) for testing

--- tests/e2e/test_notebook.py
+++ tests/e2e/test_notebook.py
@@ -3,9 +3,6 @@ import subprocess
 import os
 import shutil
 
-import nbformat
-from nbconvert.preprocessors import ExecutePreprocessor, CellExecutionError
-
 class AutoCyberE2ETest(TestCase):
     slow = 0
 
@@ -16,22 +13,13 @@ class AutoCyberE2ETest(TestCase):
             raise SkipTest('IPython is not installed')
 
         nb_file = os.path.join(os.path.dirname(__file__), "AutoCyberTutorial.ipynb")
-        nb_out_file = os.path.join(os.path.dirname(__file__), "AutoCyberTutorial_output.ipynb")
-        autocyber_folder = "examples/AutomotiveCybersecurity/"
+        nb_folder = "examples/AutomotiveCybersecurity/"
 
-        with open(nb_file) as f:
-            nb = nbformat.read(f, as_version=4)
-
-        ep = ExecutePreprocessor(timeout=600)
+        shutil.copy2(nb_file, nb_folder)
 
         try:
-            ep.preprocess(nb, {'metadata': {'path': autocyber_folder}})
-        except CellExecutionError:
-            out = None
-            msg = 'Error executing the notebook "%s".\n\n' % nb_file
-            msg += 'See notebook "%s" for the traceback.' % nb_out_file
-            print(msg)
-            raise
+
+            subprocess.check_call(['ipython', "AutoCyberTutorial.ipynb"], cwd=nb_folder)
         finally:
-            with open(nb_out_file, mode='w', encoding='utf-8') as f:
-                nbformat.write(nb, f)        
+            # not to make this tutorial folder dirty
+            os.unlink(os.path.join(nb_folder, "AutoCyberTutorial.ipynb"))
