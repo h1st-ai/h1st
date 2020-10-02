fcd939c54e978147aee7f8a0d5e00e1e6b718d8d
timroz24 <rozario.timothy@gmail.com>
2020-10-02 10:31:15 -0700

    Made Lime & Shap variables private

--- h1st/core/trust/lime_model_explainer.py
+++ h1st/core/trust/lime_model_explainer.py
@@ -1,29 +1,30 @@
 import numpy as np
 import lime.lime_tabular as lt
 
+
 class LIMEModelExplainer:
     def __init__(self, decision, model, data):
         self._model = model
         self._data = data
-        self.feature_names = list(self._data["train_df"].columns)
-        self.decision_input = decision[0]
+        self._feature_names = list(self._data["train_df"].columns)
+        self._decision_input = decision[0]
         self._lime_explainer()
         self._explain_decision()
         # self._plot_lime_explanation()
 
     def _plot_lime_explanation(self):
-        self.explanation.show_in_notebook(show_table=True, show_all=False)
+        self._explanation.show_in_notebook(show_table=True, show_all=False)
 
     def _explain_decision(self):
-        self.explanation = self.explainer.explain_instance(
-            self.decision_input,
+        self._explanation = self._explainer.explain_instance(
+            self._decision_input,
             self._model.predict,
         )
 
     def _lime_explainer(self):
-        self.explainer = lt.LimeTabularExplainer(
+        self._explainer = lt.LimeTabularExplainer(
             np.array(self._data["train_df"]),
-            feature_names=self.feature_names,
+            feature_names=self._feature_names,
             verbose=False,
             mode="regression",
         )
--- h1st/core/trust/shap_model_describer.py
+++ h1st/core/trust/shap_model_describer.py
@@ -1,20 +1,20 @@
 import shap
 
+
 class SHAPModelDescriber:
     def __init__(self, model, data):
         self._model = model
-        self.data = data
+        self._data = data
         self._get_shap_describer()
         self._get_shap_values()
         # self._plot_shap_describer()
 
     def _plot_shap_describer(self):
-        shap.summary_plot(self.shap_values, self.data['train_df'])
+        shap.summary_plot(self._shap_values, self._data['train_df'])
 
     def _get_shap_describer(self):
-        self.shap_describer = shap.TreeExplainer(self._model)
+        self._shap_describer = shap.TreeExplainer(self._model)
 
     def _get_shap_values(self):
-        self.shap_values = self.shap_describer.shap_values(
-            self.data["train_df"], check_additivity=False
-        )
+        self._shap_values = self._shap_describer.shap_values(
+            self._data["train_df"], check_additivity=False)
