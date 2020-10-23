1ba9d6786e40c7e0fdd47457b78d1ce34cdaf1c2
Zooey Nguyen <zooeyn@gmail.com>
2020-10-23 14:53:09 -0700

    Update Profile class to take in protected attributes as a list
    STILL BROKEN: this is for debugging purposes

--- h1st/core/trust/profile.py
+++ h1st/core/trust/profile.py
@@ -1,3 +1,8 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import seaborn as sns
+
 class Profile:
     """
     A Profile explores bias in a dataset w.r.t. a protected_attribute (e.g. race, gender,
@@ -7,9 +12,7 @@ class Profile:
     and non-protected.
     """
 
-    def __init__(self, data, target, protected_attribute):
-        # make protected_attributes a list
-        # save a dictionary of dictionaries
+    def __init__(self, data, target, protected_attributes: list):
         """
         Parameters:
             data: DataFrame with features as column names.
@@ -17,22 +20,34 @@ class Profile:
             protected_attribute: The feature of the data
             that is to be protected from discrimination. This column is assumed to be categorical.
         """
-        self.data = data
-        self.target = target
-        self.protected_attribute = protected_attribute
+        self.protected_attributes = protected_attributes
+        """
+        STRATA INFORMATION
+            Outer keys: protected attributes
+            Inner keys: categories within that attribute
+            STRATA values: subsetted dataframe or subsetted vector
+            SUMMARY values: dict of the following
+                HISTOGRAM: hist object with sub-histograms per column 
+                STATISTICS: DataFrame object with per-column summary statistics
+        """
+        self.strata = {} # dict of dicts of DATAFRAMES
+        self.target = {} # dict of dicts of VECTORS
+        self.strata_summary = {} # dict of dicts of DATAFRAMES
+        self.target_summary = {} # dict of dicts of one-column DATAFRAMES
+        for attribute in self.protected_attributes:
+            categories = set(data[attribute])
+            for cat in categories:
+                index_vector = (data[attribute] == cat)
+                data_subset = data[index_vector].drop(attribute, axis=1)
+                target_subset = target[index_vector]
 
-        # Returns a dict.
-        # Keys are the category within the protected attribute
-        # Values are the corresponding subsets of the data WITHOUT the protected attribute column
-        self.stratified_data = {}
-        self.stratified_target = {}
-        categories = set(self.data[self.protected_attribute])
-        for category in categories:
-            index_vector = (self.data[self.protected_attribute] == category)
-            self.stratified_data[category] = self.data[index_vector]
-            self.stratified_target[category] = self.target[index_vector]
-        for key in self.stratified_data:
-            self.stratified_data[key] = self.stratified_data[key].drop(protected_attribute, axis=1)
+                self.strata[attribute][cat] = data_subset
+                self.strata_summary[attribute][cat]["statistics"] = data_subset.describe()
+                self.strata_summary[attribute][cat]["histogram"] = data_subset.hist()
+
+                self.target[protected_attribute][cat] = target_subset
+                self.target_summary[attribute][cat]["statistics"] = target_subset.describe()
+                self.target_summary[attribute][cat]["histogram"] = target_subset.hist()
 
     def profile(self):
         """
