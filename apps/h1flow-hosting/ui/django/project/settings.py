9f41e2dc2ca3eaa02bc400d870c07355a773ff14
Khoa Ma <khoama@gmail.com>
2021-04-25 20:42:57 -0700

    — config tempaltes path

--- apps/h1flow-hosting/ui/django/project/settings.py
+++ apps/h1flow-hosting/ui/django/project/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['../..'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
