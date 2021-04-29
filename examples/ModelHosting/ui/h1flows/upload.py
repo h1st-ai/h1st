ee446e6b20726d2fcdd00013243bca9d192646a5
Khoa Ma <khoama@gmail.com>
2021-04-28 23:08:28 -0700

    — save file to uploaded

--- examples/ModelHosting/ui/h1flows/upload.py
+++ examples/ModelHosting/ui/h1flows/upload.py
@@ -1,7 +1,8 @@
 from .mock_framework import H1StepWithWebUI
-from django.http import HttpResponse
+from django.http import JsonResponse, HttpResponseBadRequest, HttpResponseServerError
 from django.views.decorators.csrf import csrf_exempt
 import os 
+import uuid
 
 
 class Upload(H1StepWithWebUI):
@@ -10,12 +11,42 @@ class Upload(H1StepWithWebUI):
         return self.handle_post(req)
     
     def handle_post(self, req):
-        self.handle_uploaded_file(req.FILES['file'])
-        return HttpResponse("Done") 
+        file = req.FILES['file']
+        
+        if file is None:
+            return HttpResponseBadRequest({
+                "status": "Bad request"
+            })
+
+        file_id = self.handle_uploaded_file(file)
+
+        return JsonResponse({
+            "status": "OK",
+            "id": file_id
+        }) 
+
+        # try:
+        #     print(file)
+        #     self.handle_uploaded_file(file)
+
+        #     return JsonResponse({
+        #         "status": "OK",
+        #         "id": ""
+        #     }) 
+        # except:
+        #     return HttpResponseServerError({
+        #         "status": "Server error",
+        #         "message": ex
+        #     })
 
     def handle_uploaded_file(self, f):
-        dir_path = os.path.dirname(os.path.realpath(__file__))
+        file_id = str(uuid.uuid4())
+        ext = os.path.splitext(str(f))[1]
+
+        print("Saving " + ext)
         
-        with open("models", 'wb+') as destination:
+        with open("uploaded/" + file_id + ext, 'wb+') as destination:
             for chunk in f.chunks():
                 destination.write(chunk)
+
+        return file_id + ext
