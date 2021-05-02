3c8c9d9cdb6ae183d72385e9ec8a2c5eceb1f894
An Phan <phanhongan@gmail.com>
2021-05-01 17:00:42 -0700

    Add PyTorch model execution

--- examples/ModelHosting/ui/h1flows/model_executor.py
+++ examples/ModelHosting/ui/h1flows/model_executor.py
@@ -3,17 +3,18 @@ import requests
 
 
 TENSORFLOW_SERVER = "http://localhost:8501/v1/models"
+PYTORCH_SERVER = "http://localhost:8080"
 
 
 class ModelExecutor:
     @staticmethod
-    def execute(model_name, input_data, input_type='text'):
+    def execute(model_name, input_data, func, input_type):
         raise NotImplementedError
 
 
 class TensorFlowModelExecutor(ModelExecutor):
     @staticmethod
-    def execute(model_name, input_data, input_type='text'):
+    def execute(model_name, input_data, func='predictions', input_type='text'):
         server_url = '{host}/{model_name}:predict'.format(host=TENSORFLOW_SERVER, model_name=model_name)
 
         # image input
@@ -30,7 +31,7 @@ class TensorFlowModelExecutor(ModelExecutor):
             prediction = response.json()['predictions'][0]
             return prediction['classes']
         
-        elif input_type=='text':
+        elif input_type == 'text':
             # text input
             predict_request = '{"inputs" : ["%s"]}' % input_data
             response = requests.post(server_url, data=predict_request)
@@ -38,8 +39,18 @@ class TensorFlowModelExecutor(ModelExecutor):
             prediction = response.json()['outputs']['prediction']
             return prediction
 
+        raise NotImplementedError
+
 
 class PyTorchModelExecutor(ModelExecutor):
     @staticmethod
-    def execute(model_name, input_data, input_type='text'):
-        pass
+    def execute(model_name, input_data, func='predictions', input_type='image'):
+        server_url = '{host}/{func}/{model_name}'.format(host=PYTORCH_SERVER, func=func, model_name=model_name)
+        if input_type == 'image':
+            files = {
+                'data': (input_data, open(input_data, 'rb')),
+            }
+            response = requests.post(server_url, files=files)
+            return response.json()
+
+        raise NotImplementedError
