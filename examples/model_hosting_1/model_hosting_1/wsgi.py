f75d59f4e5863dfd9084a713175b65d40ba895a6
Khoa Ma <khoama@gmail.com>
2021-05-05 18:51:50 -0700

    — integrate new structure

---
+++ examples/model_hosting_1/model_hosting_1/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for model_hosting_1 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'model_hosting_1.settings')
+
+application = get_wsgi_application()
