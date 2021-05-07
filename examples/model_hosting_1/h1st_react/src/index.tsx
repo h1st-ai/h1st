2f5f0d043f024958cfa6f1d481a82c37b849a17e
Khoa Ma <khoama@gmail.com>
2021-05-07 15:54:30 -0700

    — add auth0 authentication

--- examples/model_hosting_1/h1st_react/src/index.tsx
+++ examples/model_hosting_1/h1st_react/src/index.tsx
@@ -1,18 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { store } from './app/store';
-import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { store } from "./app/store";
+import { Provider } from "react-redux";
+import * as serviceWorker from "./serviceWorker";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <Auth0Provider
+      domain="aitomatic.us.auth0.com"
+      clientId="RlwK2ZrhU2HL9Qx91uIZ6v9WKrsxfV96"
+      redirectUri={window.location.origin}
+    >
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Auth0Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
