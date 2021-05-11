e552fe0db5c8a22f9907f57ba493ec660f9fc025
Khoa Ma <khoama@gmail.com>
2021-05-11 04:59:44 -0700

    — fix user mobile menu

--- examples/model_hosting_1/h1st_react/src/layouts/App/index.tsx
+++ examples/model_hosting_1/h1st_react/src/layouts/App/index.tsx
@@ -126,7 +126,7 @@ export default function App(props: any) {
                                   >
                                     {({ active }) => (
                                       <a
-                                        href="#test"
+                                        href="#logout"
                                         className={classNames(
                                           active ? "bg-gray-100" : "",
                                           "block px-4 py-2 text-sm text-gray-700"
@@ -190,16 +190,16 @@ export default function App(props: any) {
                     <div className="flex-shrink-0">
                       <img
                         className="h-10 w-10 rounded-full"
-                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                        alt=""
+                        src={user?.picture}
+                        alt={user?.name}
                       />
                     </div>
                     <div className="ml-3">
                       <div className="text-base font-medium leading-none text-white">
-                        Tom Cook
+                        {user?.name}
                       </div>
                       <div className="text-sm font-medium leading-none text-gray-400">
-                        tom@example.com
+                        {user?.nickname}
                       </div>
                     </div>
                     <button className="ml-auto bg-gray-800 flex-shrink-0 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
@@ -213,6 +213,11 @@ export default function App(props: any) {
                         key={item}
                         href="#444"
                         className="block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-gray-700"
+                        onClick={() =>
+                          logout({
+                            returnTo: window.location.origin,
+                          })
+                        }
                       >
                         {item}
                       </a>
