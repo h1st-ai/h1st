f263467610ab9e3ee8c9ef7185ca8a2c8b50d924
Khoa Ma <khoama@gmail.com>
2021-05-17 11:25:00 -0700

    — remove secret_key

--- examples/model_hosting_1/model_hosting_1/settings.py
+++ examples/model_hosting_1/model_hosting_1/settings.py
@@ -38,7 +38,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-smibr040))l3%7_k*g-wi%003lq(4+$#jax14g!ez6&46savg0'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
