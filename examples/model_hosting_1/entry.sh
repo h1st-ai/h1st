a1fd4232ccad9d39b47614e52304785d9928345d
Khoa Ma <khoama@gmail.com>
2021-05-17 17:34:01 -0700

    — change docker script

--- examples/model_hosting_1/entry.sh
+++ examples/model_hosting_1/entry.sh
@@ -2,5 +2,5 @@
 
 cd /app
 python manage.py migrate 
-gunicorn -b :8000 --workers=100 --timeout 120 model_hosting_1.wsgi 
+gunicorn -b :8000 --workers=100 --timeout 120 model_hosting_1.wsgi:application
 
