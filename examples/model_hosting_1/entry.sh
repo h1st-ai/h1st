a16e3d9d517c07cc4c2933f7480e897c6557f5bb
Khoa Ma <khoama@gmail.com>
2021-05-17 17:47:31 -0700

    — optimize gunicorn settings

--- examples/model_hosting_1/entry.sh
+++ examples/model_hosting_1/entry.sh
@@ -2,5 +2,5 @@
 
 cd /app
 python manage.py migrate 
-gunicorn -b :8000 --workers=100 --timeout 120 model_hosting_1.wsgi:application
-
+# gunicorn -b :8000 --workers=100 --timeout 120 model_hosting_1.wsgi:application
+gunicorn -b :8000  --worker-class=gevent --worker-connections=1000 --workers=80 --timeout 120 model_hosting_1.wsgi:application
