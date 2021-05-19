9459344c62122bcc770cf3d3200f1e902620f10d
Khoa Ma <khoama@gmail.com>
2021-05-19 03:52:20 -0700

    — add loading indicator

--- examples/model_hosting_1/h1st_react/src/components/loading-indicator/index.tsx
+++ examples/model_hosting_1/h1st_react/src/components/loading-indicator/index.tsx
@@ -1,13 +1,26 @@
-export default function LoadingIndicator() {
+export default function LoadingIndicator({ size = 50 }) {
   return (
-    <svg>
-      <symbol id="icon-checked" viewBox="0 0 20 20" fill="currentColor">
-        <path
-          fill-rule="evenodd"
-          d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-          clip-rule="evenodd"
-        />
-      </symbol>
+    <svg
+      width={size}
+      height={size}
+      className="animate-spin -ml-1 mr-3 h-5 w-5 text-blue-700"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+    >
+      <circle
+        className="opacity-25"
+        cx="12"
+        cy="12"
+        r="10"
+        stroke="currentColor"
+        strokeWidth="4"
+      ></circle>
+      <path
+        className="opacity-75"
+        fill="currentColor"
+        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+      ></path>
     </svg>
   );
 }
