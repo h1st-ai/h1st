1a5c8753975476a98a22309067da61033627116f
Vinh Luong <MBALearnsToCode@UChicago.edu>
2021-05-25 20:11:20 -0700

    move examples/Translation Django files

--- examples/Translation/wsgi.py
+++ examples/Translation/wsgi.py
@@ -12,6 +12,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 
 application = get_wsgi_application()
