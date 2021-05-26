2a6ce62b66c6db661bfb2d06615f459a25e882ee
Vinh Luong <MBALearnsToCode@UChicago.edu>
2021-05-25 21:44:24 -0700

    clean up examples/Translation/ai/views.py

--- examples/Translation/ai/views.py
+++ examples/Translation/ai/views.py
@@ -1,4 +1,3 @@
-from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.decorators import permission_classes
@@ -9,9 +8,6 @@ from .models.google_cloud import GoogleLanguageTranslationModel
 
 @permission_classes([AllowAny])
 class TranslationModelAPIView(APIView):
-    def get(self, request, *args, **kwargs):
-        return Response('Hello World')
-
     def post(self, request, *args, **kwargs):
         model = GoogleLanguageTranslationModel()
         result = model.predict(request.data, target='fr')
