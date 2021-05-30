bb26dc6c3624f9b83f60b607bfd6fce69a5fd889
Vu Nhan <nhanitvn@gmail.com>
2021-05-29 17:48:26 -0700

    Add Keras-based Seq2Seq Transfomer for En2Spa

--- examples/Translation/ai/tests.py
+++ examples/Translation/ai/tests.py
@@ -1,4 +1,5 @@
-from mock_framework import GoogleLanguageTranslationModel, HuggingFaceLanguageTranslationModel
+from ai.models.google_cloud import GoogleLanguageTranslationModel, HuggingFaceLanguageTranslationModel
+from ai.models.seq2seq_transformer import En2SpaSeq2SeqTransformer
 
 def test_google_translation():
     model = GoogleLanguageTranslationModel()
@@ -7,3 +8,17 @@ def test_google_translation():
 def test_hugging_face_translation():
     model = HuggingFaceLanguageTranslationModel('en', 'de')
     assert model.predict('Hello') == 'Hallo'
+
+def test_Keras_En2Spa_transformer_translation():
+
+    model = En2SpaSeq2SeqTransformer()
+    # model_folder = 'En2SpaSeq2SeqTransformer'
+    train_pairs, val_pairs, _ = model.load_data()
+    train_ds, val_ds = model.prep_data(train_pairs, val_pairs)
+
+    # This will take ~ 20 minute per epoch
+    model.train(data=(train_ds, val_ds))
+    # model persistence does not work for now
+    # model.persist(model_folder)
+
+    model.predict('She handed him the money')
