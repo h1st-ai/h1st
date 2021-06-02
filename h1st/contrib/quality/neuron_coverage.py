ac4e896e677e6a80c2279698eddf262e4b827108
Vu Nhan <nhanitvn@gmail.com>
2021-06-02 16:11:09 -0700

    Add NeuronCoverage contrib stubs

---
+++ h1st/contrib/quality/neuron_coverage.py
@@ -0,0 +1,31 @@
+import tensorflow as tf
+import time
+
+class TFNeuronCoverage1():
+    def __init__(self):
+        pass
+    def gen_diff(self, seeds, model):
+        for _ in range(1000):
+            for iter in range(100):
+                print("Generate mutants...")
+                time.sleep(1)
+                print("Computing neuron coverage....")
+                time.sleep(1)
+
+            print('Covered neurons percentage: %.2f %%' % 63)
+
+
+class TFNeuronCoverage2():
+    def __init__(self):
+        pass
+    def gen_diff(self, seeds, model):
+        for _ in range(1000):
+            for iter in range(100):
+                print("Generate mutants...")
+                time.sleep(20)
+                print("Computing neuron coverage....")
+                time.sleep(20)
+
+            print('Covered neurons percentage: %.2f %%' % 54)
+
+
