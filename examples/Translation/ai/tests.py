81641e0c53f3c8a890e42ca29d5e2ba54b0ce6c3
Vu Nhan <nhanitvn@gmail.com>
2021-06-04 14:40:34 -0700

    Update tests

--- examples/Translation/ai/tests.py
+++ examples/Translation/ai/tests.py
@@ -1,6 +1,8 @@
 from models.google_cloud import GoogleLanguageTranslationModel, HuggingFaceLanguageTranslationModel
 from models.en2spa_transformer import En2SpaSeq2SeqTransformer
 
+from h1st.contrib.quality.neuron_coverage import TFNeuronCoverage1, TFNeuronCoverage2
+
 def test_google_translation():
     model = GoogleLanguageTranslationModel()
     assert model.predict(text='Hello', target='de') == 'Hallo'
@@ -17,7 +19,7 @@ def test_En2Spa_transformer_translation():
     train_ds, val_ds = model.prep_data(data=(train_pairs, val_pairs))
 
     # This will take ~ 20 minute per epoch on CPU
-    model.train(data=(train_ds, val_ds), epochs=200)
+    model.train(data=(train_ds, val_ds), epochs=10)
     model.persist(model_version)
 
 def test_En2Spa_transformer_translation_prediction():
@@ -28,3 +30,8 @@ def test_En2Spa_transformer_translation_prediction():
 
     print(model.predict('Hello'))
 
+def test_neuron_coverage():
+    model = En2SpaSeq2SeqTransformer()
+    train_pairs, val_pairs, test_pairs = model.load_data()
+    nc1 = TFNeuronCoverage1()
+    nc1.gen_diff(seeds=test_pairs, model=model)
