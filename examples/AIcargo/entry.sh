ee1961038a33dfb5f1d4e380602de9f30f95b614
Shiti Saxena <ssaxena.ece@gmail.com>
2021-07-15 08:34:54 -0700

    refactor: remove aicargo from examples

--- examples/AIcargo/entry.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd /app
-python manage.py migrate 
-# gunicorn -b :8000 --workers=100 --timeout 120 model_hosting_1.wsgi:application
-gunicorn -b :8000  --worker-class=gevent --worker-connections=1000 --workers=80 --timeout 120 model_hosting_1.wsgi:application
--- examples/AIcargo/h1st_api/controllers/mocked/model_step.py
+++
@@ -1,9 +0,0 @@
-from .step import H1Step
-
-
-class H1ModelStep(H1Step):
-    def __init__(self, model_id, model_platform, model_path=None, model_version=1):
-        self.model_id = model_id
-        self.model_platform = model_platform
-        self.model_path = model_path
-        self.model_version = model_version
