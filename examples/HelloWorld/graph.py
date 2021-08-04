4c655f6b1ae9b9854259dddeeb914222f154c26f
Shiti Saxena <ssaxena.ece@gmail.com>
2021-08-04 14:39:56 -0700

    fix[helloworld]: correct imports in code and tests

--- examples/HelloWorld/graph.py
+++ examples/HelloWorld/graph.py
@@ -3,11 +3,14 @@ This is an example of a very simple graph which prints hello for each even numbe
 using a conditional RuleBasedModel node and a HelloPrinter h1.Action.
 """
 
-import h1st.core as h1
-from rule_based_model import RuleBasedModel
+from h1st.h1flow.h1flow import Graph
+from h1st.h1flow.h1step import Action, Decision, NoOp
+from rule_based_model import SimpleRuleBasedModel
 
-class HelloPrinter(h1.Action):
+
+class HelloPrinter(Action):
     """Print hello to the inputs value"""
+
     def call(self, command, inputs):
         # Note that H1st does the conditional/filtering orchestration already.
         # All we need to do here is just to print.
@@ -18,16 +21,17 @@ class HelloPrinter(h1.Action):
 def create_graph():
     """Create a graph which prints hello for each even number x in the input stream,
     using a conditional RuleBasedModel node and a HelloPrinter h1.Action."""
-    graph = h1.Graph()
-    graph.start()\
-        .add(h1.Decision(RuleBasedModel(), result_field="predictions"))\
-        .add(yes=HelloPrinter(), no=h1.NoOp())
+    graph = Graph()
+    graph.start() \
+        .add(Decision(SimpleRuleBasedModel(), result_field="predictions")) \
+        .add(yes=HelloPrinter(), no=NoOp())
     graph.end()
     return graph
 
+
 if __name__ == "__main__":
-    graph = create_graph()
-    results = graph.predict({"values": range(6)})
+    generated_graph = create_graph()
+    results = generated_graph.predict({"values": range(6)})
     # Should get:
     # Hello world 0!
     # Hello world 2!
