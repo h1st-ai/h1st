c1dbc5027d3ed7fec11ca56878df5baf6ff9bef4
Shiti Saxena <shiti@aitomatic.com>
2021-12-14 12:27:26 -0800

    refactor: remove old code
    BREAKING CHANGE: old graph and flow apis have been removed

--- h1st/model/rule_based_model.py
+++
@@ -1,6 +0,0 @@
-from h1st.model.model import Model
-from .predictive_model import PredictiveModel
-
-
-class RuleBasedModel(PredictiveModel):
-    pass
--- h1st/trust/debiasable.py
+++
@@ -1,7 +0,0 @@
-class Debiasable:
-    """
-    [summary]
-    """
-
-    def debias(self, decisions):
-        pass # to be implemented
