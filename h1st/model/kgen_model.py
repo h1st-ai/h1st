478e332656eff628d53f8ff48bbcb5b54088e2b6
Vu Nhan <nhanitvn@gmail.com>
2021-12-29 11:50:28 -0800

    feat: add Knowledge Generalizer/Ensemble Modeler/Model

---
+++ h1st/model/kgen_model.py
@@ -0,0 +1,7 @@
+from h1st.model.ml_model import MLModel
+
+class KGenModel(MLModel):
+    """
+    Knowledge Generalization Model
+    """
+    pass
---
+++ h1st/model/kgen_modeler.py
@@ -0,0 +1,7 @@
+from h1st.model.ml_modeler import MLModeler
+
+class KGenModeler(MLModeler):
+    """
+    Knowledge Generalization Modeler
+    """
+    pass
