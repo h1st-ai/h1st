a098f147b25f0f620be4a1fd165decd9b4f14d84
Christopher Nguyen <ctn@aitomatic.com>
2021-12-31 00:10:34 -0800

    Using poetry and removing unnecessary config files.

--- user/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# Don’t forget to import __init__
-# Workaround for our inability to run poetry editable environment on Mac M1 for the moment.
-
-__local_dev = True
-
-if __local_dev:
-    # Include h1st/ in the system path for library imports. Will be slower.
-    import sys, os
-    sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
