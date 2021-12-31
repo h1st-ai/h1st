01e70477d8566f45a8e46d8b9ab63cbac013b21c
Christopher Nguyen <ctn@users.noreply.github.com>
2021-12-31 04:48:38 -0800

    Update modelable.py

--- h1st/model/modelable.py
+++ h1st/model/modelable.py
@@ -2,5 +2,10 @@
 class Modelable:
     """
     Base class for all classes that are operated-upon by Modeler.
+    
+    classDiagram
+    Model ..|> Modelable
+    Modelable <.. Modeler
+    
     """
     pass
