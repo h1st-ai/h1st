708dfdfa42901afcc2bd103ca7dfca44671419a4
Vu Nhan <nhanitvn@gmail.com>
2022-01-18 10:33:42 -0800

    feat: Add basic Oracle for binary, tabular data

--- h1st/model/oracle/oracle.py
+++ h1st/model/oracle/oracle.py
@@ -1,6 +1,6 @@
+from typing import Dict, NoReturn
 from h1st.model.predictive_model import PredictiveModel
 
-from .teacher import Teacher
 from .student import Student, StudentModeler
 from .ensemble import Ensemble
 
@@ -65,6 +65,41 @@ end Note
 
 class Oracle(PredictiveModel):
     """
-    Teacher Model in Oracle framework
+    Oracle Model in Oracle framework
     """
-    pass
+    def __init__(self, knowledge_model: PredictiveModel, student_modeler: StudentModeler = StudentModeler, student: Student = Student, ensemble: Ensemble = Ensemble):
+        self.metas = {}
+        self.teacher = knowledge_model
+        self.ensemble = ensemble()
+        self.student_modeler = student_modeler()
+        self.student_modeler.model_class = student
+
+    def build(self, data:Dict) -> NoReturn:
+        # Generate knowledge_model's prediction
+        teacher_pred = self.teacher.predict(data)['pred']
+
+        # Train the student model
+        self.student = self.student_modeler.build_model({'X': data['X'], 'y': teacher_pred})
+
+    def predict(self, input_data: dict) -> dict:
+        if not hasattr(self, 'student'):
+            raise RuntimeError('No student built')
+
+        # Generate knowledge_model's prediction
+        teacher_pred = self.teacher.predict(input_data)
+        # Generate student model's prediction
+        student_pred = self.student.predict(input_data)
+
+        return self.ensemble.predict({'teacher_pred': teacher_pred['pred'], 'student_pred': student_pred['pred']})
+
+    def persist(self, version=None):
+        if not hasattr(self, 'student'):
+            raise RuntimeError('No student built')
+        
+        version = self.student.persist(version)
+        super().persist(version)
+
+    def load_params(self, version: str = None) -> None:
+        self.student = self.student_modeler.model_class()
+        self.student.load_params(version)
+        super().load_params(version)
