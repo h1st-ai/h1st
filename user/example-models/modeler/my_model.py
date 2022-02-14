aaf9155ae5d183e0ad7bae67557bad1de2100a83
Khoa Ma <khoama@gmail.com>
2022-02-14 14:48:51 -0800

    adjust my_model example

--- user/example-models/modeler/my_model.py
+++ user/example-models/modeler/my_model.py
@@ -1,12 +1,22 @@
 import __init__
+from typing import Dict
 from h1st.model.model import Model
 
 class MyModel(Model):
-   def process(self):
-       return {"key": "value"}
+    def process(self, data:dict = None) -> dict:
+        return {"key": "value"}
 
 
 if __name__ == "__main__":
-    m = MyModel.get_modeler().build_model()
-    result = m.process()
+    modeler = MyModel.get_modeler()
+    model = modeler.build()
+    
+    modeler.persist(model, 'my_model_id')
+
+    result = model.process()
+    print(result)
+
+    model = modeler.load_model('my_model_id')
+
+    result = model.process()
     print(result)
