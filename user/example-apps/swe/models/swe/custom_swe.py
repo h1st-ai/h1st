40bfefb84c3ecd3129598c1fcb37bf124e5f6fdf
Taejin Chun <tjjeon108@gmail.com>
2022-04-20 12:19:33 +0900

    updated user code of kswe

--- user/example-apps/swe/models/swe/custom_swe.py
+++ user/example-apps/swe/models/swe/custom_swe.py
@@ -4,11 +4,21 @@ import pandas as pd
 
 from sub_model import MySubModelModeler, MyEnsembleModeler
 import utils
-from h1st.model.swe import SegmentedWorldEnsembleModeler, SegmentedWorldEnsemble, Segmentor
-
+from h1st.model.kswe import KSWEModeler, KSWE
+from h1st.model.model import Model
 '''
-This is the user code of custom SegmentedWorldEnsembleModeler which use 
-custom MySegmentor, MySubModelModeler, and MyEnsembleModeler.
+This is the user code of custom KSWEModeler. We demonstrate here how users can 
+fully customize the segmentor for segmenting data and use it in KSWEModeler. 
+If users want to use custom SubModel and Ensemble, they can use their
+customized SubModelModeler and EnsembleModeler and we will show you how to use
+them in KSWE as well. 
+In Summary, overall process is like the following. 
+1. Load entire data
+2. Build custom Segmentor, SubModelModeler, and EnsembleModeler
+3. Pass them to KSWEModeler.build_model() method. 
+4. Check (auto-generated) evaluation results and persist KSWE if the evaluation
+    results is good enough. Otherwise, iterate the fine-tuning process. 
+5. Load and use KSWE in inference application
 '''
 
 ### 1. Load Data
@@ -17,48 +27,51 @@ data = utils.load_data()
 ### 2. Build Custom Segmentor
 '''
 User build a custom Segmentor that will be used to segment the entire dataset.
-User should implement the following methods
+User should implement the following two methods
 1. segment(data: Dataframe or Dict, by: Dict) -> Dict:
-    This method splits data using the by value. 
-    This method returns two variables, segmented_data and filter_combinations. 
+    This is where users can implement data segmentation logics. Using your 
+    segmentation_logics, this method generates segmentation_results which includes
+    data segments with their names.
+    This method should return two variables, segmentation_results and segmentation_logics.
+    
     Their format is like the following.
-    - segmented_data: Dict[segment_name: str, data_segment: pd.DataFrame or Dict]
-    - filter_combinations: Dict[segment_name: str, list_of_filters: List[(feature_name, filter)]] 
-      Each filter can be a list or tuple.
-        1. list - Feature should have one of the values in the list 
-        2. tuple(start, end) - Feature should be in the range of [start: end). 
+    - segmentation_results: Dict[segment_name: str, data_segment: Union[pd.DataFrame, Dict]]
+    - segmentation_logics: Dict[segment_name: str, list_of_segmentation_logic: List[(feature_name, logic)]] 
+      Each logic can be a list or tuple.
+        1. list - Make a segment that has one of the values in the list in the feature column
+        2. tuple(start, end) - Make a segment whose values of feature column are in the range of [start: end). 
             If start or end is None, that means no lower or upper boundary in a range.
     The example of them are provided in MySegmentor class. 
 
-2. identify_segment(data, filter_combinations):
+2. identify_segment(data, segmentation_logics):
     This method finds the proper segment of provided data and return its
-    segment_name. When you use SWE in inference phase, based on this segment info, 
+    segment_name. When you use KSWE in inference phase, based on this segment info, 
     you can select or give more weight on relevant models. If you implement this logic
     inside Ensemble itself, then you don't need to implement this method. 
 '''
-class MySegmentor(Segmentor):
+class MySegmentor(Model):
     def segment(data: Union[pd.DataFrame, Dict]) -> Tuple[Dict]:
-        segmented_data = {
+        segmentation_results = {
             'segment_0': data_segment_0,
             'segment_1': data_segment_1
         }
-        filter_combinations = {
-            'segment_0': [('sepal_size', (None, 18.5)),('sepal_aspect_ratio', (None, 0.65))],
-            'segment_1': [('sepal_size', (18.5, None)),('species', [0, 1])],
+        segmentation_logics = {
+            'segment_0': [('sepal_size', (2, 18.5)),('species', [0, 1])],
+            'segment_1': [('sepal_size', (18.5, 30)),('species', [0, 1])],
         }
-        return (segmented_data, filter_combinations)
-        
-    def identify_segment(data: Union[pd.DataFrame, Dict], filter_combinations: Dict) -> List:
+        return (segmentation_results, segmentation_logics)
+
+    def identify_segment(data: Union[pd.DataFrame, Dict], segmentation_logics: Dict) -> List:
         segment_names = ['segment_0']
         return segment_names
 
-### 3. Instantiate SWE Modeler
-swe_modeler = SegmentedWorldEnsembleModeler()
+### 3. Instantiate KSWEModeler
+kswe_modeler = KSWEModeler()
 
-### 4. Build SWE
+### 4. Build KSWE
 '''
 input_data: Dict 
-    Entire data for training the sub_models and ensembles of SWE. 
+    Entire data for training the sub_models and ensembles of KSWE. 
 sub_model_modeler:  
     User can use their own sub_model modeler, or use pre-built modeler, 
     such as RandomForestClassifierModeler.
@@ -68,7 +81,7 @@ ensemble_modeler:
 segmentor:
 
 '''
-swe = swe_modeler.build_model(
+kswe = kswe_modeler.build_model(
     input_data=data,
     sub_model_modeler=MySubModelModeler,
     ensemble_modeler=MyEnsembleModeler,
@@ -76,29 +89,29 @@ swe = swe_modeler.build_model(
 )
 
 ### 5. Check evalutaion results
-print(swe.metrics)
+print(kswe.metrics)
 
-### 6. Persist SWE
-swe.persist(swe, 'my_version')
+### 6. Persist KSWE
+kswe.persist(kswe, 'my_version')
 
-### 7. Reload SWE in Application
-swe = SegmentedWorldEnsemble().load_param('my_version')
-swe.predict(data)
+### 7. Reload KSWE in Application
+kswe = KSWE().load_param('my_version')
+kswe.predict(data)
 
 
 # ============= After merging model-refactoring branch into main branch
 
-### 3. Instantiate SWE Modeler
-swe_modeler = SegmentedWorldEnsemble.get_modeler()
+### 3. Instantiate KSWE Modeler
+kswe_modeler = KSWE.get_modeler()
 
-### 4. Build SWE - same
+### 4. Build KSWE - same
 
 ### 5. Check evalutaion results - same
 
-### 6. Persist SWE
-swe_modeler.persist(swe, 'my_version')
+### 6. Persist KSWE
+kswe_modeler.persist(kswe, 'my_version')
 
-### 7. Reload SWE in Application
-swe = SegmentedWorldEnsemble().get_modeler().load('my_version')
-swe.predict(data)
+### 7. Reload KSWE in Application
+kswe = KSWE().get_modeler().load('my_version')
+kswe.predict(data)
 
--- user/example-apps/swe/models/swe/default_swe.py
+++ user/example-apps/swe/models/swe/default_swe.py
@@ -1,10 +1,21 @@
 import utils
-from h1st.model.swe import SegmentedWorldEnsembleModeler, SegmentedWorldEnsemble
-from h1st.model.swe import RandomForestClassifierModeler, MajorityVotingEnsembleModeler
+from h1st.model.kswe import KSWEModeler, KSWE
+from h1st.model.kswe import RandomForestClassifierModeler, MajorityVotingEnsembleModeler
 
 '''
-This is the user code of default SegmentedWorldEnsembleModeler which
-use CombinationSegmentor as a default Segmentor.
+This is the user code of default KSWEModeler which use CombinationSegmentor 
+as a default Segmentor. The overall process is like the following. 
+1. load data
+2. provide segmentation_features that explains what features users want to
+  use to generate data segments from the original data
+3. build sub_model_modeler and ensemble_modeler or just use pre-built
+  modelers from h1st
+4. Pass them to KSWEModeler.build_model() method. 
+5. Check (auto-generated) evaluation results and persist KSWE if the evaluation
+    results is good enough. Otherwise, iterate the fine-tuning process. 
+6. Load and use KSWE in inference application
+
+
 
 '''
 ### 1. Load Data
@@ -21,42 +32,24 @@ a list or tuple.
 '''
 segmentation_features = utils.load_segmentation_features('.ini file path')
 segmentation_features = {
-    'sepal_size': [(None, 18.5), (18.5, None)],
+    'sepal_size': [(0, 18.5), (18.5, None)],
     'sepal_aspect_ratio': [(0, 0.65), (0.65, 1)],
     'species': [[0, 1]]
 }
 
-### 3. Instantiate SWE Modeler
-swe_modeler = SegmentedWorldEnsembleModeler()
+### 3. Instantiate KSWE Modeler
+kswe_modeler = KSWEModeler()
 
-### 4. Build SWE
+### 4. Build KSWE
 '''
 input_data: Dict 
-    Entire data for training the sub_models and ensembles of SWE. 
+    Entire data for training/evaluating the sub_models and ensembles of KSWE. 
 segmentation_features: Dict
     Explained above. 
-segmentation_levels: List
-    Number of features being used to create each segment. If level=1, then
-    take a filter from only one featrue to create a segment. If level=2, then 
-    take a filter from each feature and create a combined filter to create a 
-    segment. To create a combined filter, do and operation between two filters.
-    
-    For example, given an above segmentation_features, if level=3, then you will
-    have the following 4 segments.
-        segment_1: (sepal_size, (None, 18.5)) & (sepal_aspect_ratio, (0, 0.65)) & (species, [0, 1])
-        segment_2: (sepal_size, (None, 18.5)) & (sepal_aspect_ratio, (0.65, 1)) & (species, [0, 1])
-        segment_3: (sepal_size, (18.5, None)) & (sepal_aspect_ratio, (0, 0.65)) & (species, [0, 1])
-        segment_4: (sepal_size, (18.5, None)) & (sepal_aspect_ratio, (0.65, 1)) & (species, [0, 1])
-    If level=1, then you will have the following 5 segments.
-        segment_1: (sepal_aspect_ratio, (0, 0.65))
-        segment_2: (sepal_aspect_ratio, (0.65, 1))
-        segment_3: (sepal_size, (None, 18.5))
-        segment_4: (sepal_size, (18.5, None))
-        segment_5: (species, [0, 1])
-min_data_size: 
+min_segment_size: 
     Number of data points in each segment should be larger than this value. 
     If not, ignore that segment or auto-merge it into other segment so that
-    the total number data points in a merged segment is larger than min_data_size.  
+    the total number data points in a merged segment is larger than min_segment_size.  
 sub_model_modeler:  
     User can use their own sub_model modeler, or use pre-built modeler, 
     such as RandomForestClassifierModeler.
@@ -64,39 +57,38 @@ ensemble_modeler:
     User can use their own ensemble modeler, or use pre-built modeler, 
     such as MajorityVotingEnsembleModeler.
 '''
-swe = swe_modeler.build_model(
+kswe = kswe_modeler.build_model(
     input_data=data,
     segmentation_features=segmentation_features,
-    segmentation_levels=[1, 2],
-    min_data_size=50, 
+    min_segment_size=50, 
     sub_model_modeler=RandomForestClassifierModeler,
     ensemble_modeler=MajorityVotingEnsembleModeler
 )
 
 ### 5. Check evalutaion results
-print(swe.metrics)
+print(kswe.metrics)
 
-### 6. Persist SWE
-swe.persist(swe, 'my_version')
+### 6. Persist KSWE
+kswe.persist(kswe, 'my_version')
 
-### 7. Reload SWE in Application
-swe = SegmentedWorldEnsemble().load_param('my_version')
-swe.predict(data)
+### 7. Reload KSWE in Application
+kswe = KSWE().load_param('my_version')
+kswe.predict(data)
 
 
 # ============= After merging model-refactoring branch into main branch
 
-### 3. Instantiate SWE Modeler
-swe_modeler = SegmentedWorldEnsemble.get_modeler()
+### 3. Instantiate KSWE Modeler
+kswe_modeler = KSWE.get_modeler()
 
-### 4. Build SWE - same
+### 4. Build KSWE - same
 
 ### 5. Check evalutaion results - same
 
-### 6. Persist SWE
-swe_modeler.persist(swe, 'my_version')
+### 6. Persist KSWE
+kswe_modeler.persist(kswe, 'my_version')
 
-### 7. Reload SWE in Application
-swe = SegmentedWorldEnsemble().get_modeler().load('my_version')
-swe.predict(data)
+### 7. Reload KSWE in Application
+kswe = KSWE().get_modeler().load('my_version')
+kswe.predict(data)
 
