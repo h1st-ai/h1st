ecb512c655edac0e3d55ac9bde41bfd53f97da8f
Taejin Chun <tjjeon108@gmail.com>
2022-04-27 13:27:22 +0900

    added prepare_sub_model_train_test_data method in kswe_modeler

--- h1st/model/kswe/__init__.py
+++ h1st/model/kswe/__init__.py
@@ -1,5 +1,5 @@
 from .kswe import KSWE
 from .kswe_modeler import KSWEModeler
-from .segmentor import Segmentor, CombinationSegmentor
-from .ensemble import Ensemble, MajorityVotingEnsembleModeler
+from .segmentor import Segmentor, CombinationSegmentor, MaxSegmentationModeler
+from .ensemble import Ensemble, MajorityVotingEnsemble, MajorityVotingEnsembleV2
 from .sub_model_modeler import RandomForestClassifierModeler
--- h1st/model/kswe/kswe_modeler.py
+++ h1st/model/kswe/kswe_modeler.py
@@ -54,37 +54,38 @@ class KSWEModeler(Modeler):
             Alternatively, this can be string path to config file, see example for format.
         '''
         if 'X_train' not in input_data or 'X_test' not in input_data:
-            raise KeyError('key "X_train" or "X_test" are not in your input_data')            
-        if 'y_train' not in input_data or 'y_test' not in input_data: 
+            raise KeyError('key "X_train" or "X_test" are not in your input_data')
+
+        if bool('y_train' in input_data) ^ bool('y_test' in input_data): 
             raise KeyError('key "y_train" or "y_test" are not in your input_data')
+
         if not isinstance(input_data['X_train'], pd.DataFrame):
             raise TypeError(f'input_data["X_train"] should be pandas.DataFrame \
-                            but {type(input_data["X_train"])} were given')    
-        if (input_data['X_train'].shape[0] != input_data['y_train'].shape[0]) \
-            or (input_data['X_test'].shape[0] != input_data['y_test'].shape[0]):
-            raise ValueError('The length of "X" and "y" are different.')
-        # self.stats['segmentation_features'] = list(segmentation_config.keys())
+                            but {type(input_data["X_train"])} were given')
+        
+        if 'y_train' in input_data and 'y_test' in input_data:
+            if input_data['X_train'].shape[0] != input_data['y_train'].shape[0]:
+                raise ValueError(f'The length of X_train {input_data["X_train"].shape[0]}'
+                                f'and y_train {input_data["y_train"].shape[0]} are different.')
+            if input_data['X_test'].shape[0] != input_data['y_test'].shape[0]:
+                raise ValueError(f'The length of X_test {input_data["X_test"].shape[0]}'
+                                f'and y_test {input_data["y_test"].shape[0]} are different.')                    
 
         segmentor = segmentor_modeler.build_model(input_data, segmentation_config)
         segmentor_output = segmentor.process({'X': input_data['X_train']})
 
         sub_models = {}
         segmented_data = {}
-        train_test_data = {
-            'X_test': deepcopy(input_data['X_test']),
-            'y_test': deepcopy(input_data['y_test'])        
-        }
-        for name, X_train in segmentor_output['segment_data'].items():
-            y_train = input_data['y_train'].loc[X_train.index]
-            if y_train.nunique() == 1:
+        for name, segmented_X_train in segmentor_output['segment_data'].items():
+            train_test_data = self.prepare_sub_model_train_test_data(
+                name, segmented_X_train, input_data)
+            if ('y_train' in train_test_data) and (train_test_data['y_train'].nunique() == 1):
                 logging.info(f'Skip the Training of sub_model {name} because '
-                             'there is only one y class.')
+                             'there is only one y class or one constant value.')
                 continue
-            train_test_data['X_train'] = X_train
-            train_test_data['y_train'] = y_train
-            segmented_data[name] = {'X_train': X_train, 'y_train': y_train}
+            segmented_data[name] = train_test_data
             logging.info(f'Training sub_model {name} based on '
-                         f'{X_train.shape[0]} samples')
+                         f'{segmented_X_train.shape[0]} samples')
             sub_model = sub_model_modeler.build_model(train_test_data)
             sub_model.stats['segment_info'] = segmentor_output['segment_info'][name]
             sub_models[name] = sub_model
@@ -113,19 +114,30 @@ class KSWEModeler(Modeler):
         return kswe
 
     def build_ensemble(self, ensemble_modeler, sub_models, segmented_data):
-        # Generate sub_model predictions
-        sub_model_predictions = {
-            name: sub_model.predict({'X': segmented_data[name]['X_train']})['predictions']
-            for name, sub_model in sub_models.items()
-        }
+        ensemble_training_data = {}
+        for name, sub_model in sub_models.items():
+            data_segment = segmented_data[name]
 
-        # format training Y in same way
-        y_train = {name: data['y_train'] for name, data in segmented_data.items()}
-
-        # Build Ensemble
-        ensemble_data = {
-            'X_train': sub_model_predictions,
-            'y_train': y_train,
-        }
-        ensemble = ensemble_modeler.build_model(ensemble_data)
+            # Generate sub_model predictions
+            sub_model_pred = sub_model.predict(
+                {'X': data_segment['X_train']})['predictions']
+            ensemble_training_data[name] = {'X_train': sub_model_pred}
+            
+            # If y_train exists, add it in training data
+            if 'y_train' in data_segment:
+                ensemble_training_data[name] = {'y_train': data_segment['y_train']}
+        
+        # Build Ensemble            
+        ensemble = ensemble_modeler.build_model(ensemble_training_data)
         return ensemble
+
+    def prepare_sub_model_train_test_data(self, name, X_train, input_data):
+        train_test_data = {
+            'X_train': X_train,
+            'X_test': deepcopy(input_data['X_test']),
+        }
+        if 'y_test' in input_data:
+            train_test_data['y_train'] = input_data['y_train'].loc[X_train.index]
+            train_test_data['y_test'] = deepcopy(input_data['y_test'])
+        return train_test_data
+            
