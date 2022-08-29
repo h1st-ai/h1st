cb357b16cde4a725410fa1f039bd3d6ffbed8bcb
Tuan Pham <phamhoangtuan.it@gmail.com>
2022-08-29 15:09:56 +0700

    refactor: update init function to add more variables, change func name from execute_rules to predict

--- h1st/model/fuzzy/fuzzy_model.py
+++ h1st/model/fuzzy/fuzzy_model.py
@@ -1,6 +1,6 @@
-import imp
 import logging
 from typing import Dict
+from skfuzzy import control as skctrl
 
 from h1st.model.rule_based_model import RuleBasedModel
 from h1st.model.fuzzy.fuzzy_variables import FuzzyVariables
@@ -12,20 +12,27 @@ logger = logging.getLogger(__name__)
 
 class FuzzyModel(RuleBasedModel):
     """
-    FuzzyModel is a RuleBasedModel. You can create FuzzyModel using 
+    FuzzyModel is a RuleBasedModel. You can create FuzzyModel using
     build_model method of FuzzyModeler. FuzzyModel has a property called
-    rules and it contains the core ControlSystem from skfuzzy package. 
+    rules and it contains the core ControlSystem from skfuzzy package.
     For more information, check out https://scikit-fuzzy.github.io/scikit-fuzzy/.
-
     """
-    def __init__(self):
-        super().__init__()
-        self.fuzzy_variables: FuzzyVariables = None
-        self.fuzzy_rules: FuzzyRules = None
 
-    def execute_rules(self, input_data: Dict) -> Dict:
+    def __init__(self, variables: FuzzyVariables = None, rules: FuzzyRules = None):
+        super().__init__()
+        self.fuzzy_variables = variables
+        self.fuzzy_rules = rules
+
+        if rules is None:
+            self.rules = None
+        else:
+            self.rules = skctrl.ControlSystemSimulation(
+                skctrl.ControlSystem(rules.get_rules())
+            )
+
+    def predict(self, input_data: Dict) -> Dict:
         """
-        Execute rules on input_data. 
+        Execute rules on input_data.
 
         .. code-block:: python
             :caption: example
@@ -33,8 +40,12 @@ class FuzzyModel(RuleBasedModel):
             predictions = model.execute_rules(input_data)
         """
         if self.rules is None:
-            raise ValueError(('Property rules is None. Please load your rules '
-                              'to run this method.'))
+            raise ValueError(
+                (
+                    'Property rules is None. Please load your rules '
+                    'to run this method.'
+                )
+            )
         for key, value in input_data.items():
             self.rules.input[key] = value
         self.rules.compute()
