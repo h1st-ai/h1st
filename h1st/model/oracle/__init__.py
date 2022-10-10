91c600d1cf2680277e7ee8047a1d37e7781ec291
Tuan Pham <phamhoangtuan.it@gmail.com>
2022-10-10 10:32:44 +0700

    fix: update tutorial to use new Oracle code

--- h1st/model/oracle/__init__.py
+++ h1st/model/oracle/__init__.py
@@ -1,2 +1,4 @@
 from h1st.model.oracle.oracle_modeler import OracleModeler
 from h1st.model.oracle.oracle_model import OracleModel
+from h1st.model.oracle.ts_oracle_modeler import TimeseriesOracleModeler
+from h1st.model.oracle.ts_oracle_model import TimeSeriesOracleModel
