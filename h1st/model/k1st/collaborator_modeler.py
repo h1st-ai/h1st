4c6b5084dca485b79ffd8f0177934565ab52957e
Roshan Nanu <roshan.nanu@gmail.com>
2023-01-04 00:53:06 -0600

    feat: create multi_model wrapper and kCollaborator (#173)
    * feat: create multi_model wrapper and kCollaborator
    
    * fix: type annotations
    
    * fix: debug collaborator & multi model
    
    * feat: add compelete user code for collaborator and multi model

---
+++ h1st/model/k1st/collaborator_modeler.py
@@ -0,0 +1,95 @@
+import logging
+from typing import List
+
+import pandas as pd
+
+from h1st.model.wrapper.multi_modeler import MultiModeler
+from h1st.model.oracle.ensembler_models import MajorityVotingEnsembleModel
+from h1st.model.rule_based_modeler import RuleBasedModeler
+from h1st.model.k1st.collaborator import kCollaboratorModel
+from h1st.model.predictive_model import PredictiveModel
+from h1st.model.modeler import Modeler
+from h1st.model.model import Model
+from h1st.model.ml_modeler import MLModeler
+
+
+class kCollaboratorModeler(MultiModeler):
+
+    model_class = kCollaboratorModel
+
+    def __init__(self):
+        super().__init__()
+
+    def build_model(self, prepared_data: dict,
+                    modelers: List[MLModeler]=[],
+                    ensemble_modeler: Modeler = RuleBasedModeler(MajorityVotingEnsembleModel),
+                    models: List[PredictiveModel]=None,
+                    inject_x_in_ensembler: bool=False) -> kCollaboratorModel:
+        '''
+        prepared_data must be in the format necessary for modelers
+        '''
+        self.stats['inject_x_in_ensembler'] = inject_x_in_ensembler
+        model = super().build_model(prepared_data, modelers)
+        for i, m in enumerate(models):
+            model.add_model(m, name=f'prebuilt-{model.__class__.__name__}-{i}')
+
+        # train ensemble
+        raw_pred = model.predict(
+            {model.data_key: prepared_data['x_train']}
+        )[model.output_key]
+
+        # If there is labeled_data and ensembler_modeler is MLModeler,
+        # then prepare the training data of ensembler.
+        labeled_data = prepared_data.get("labeled_data", None)
+        if isinstance(ensemble_modeler, MLModeler) and labeled_data is None:
+            raise ValueError("No data to train the machine-learning-based ensembler")
+
+        ensembler_data = {}
+        if labeled_data:
+            x_train_input = {"x": labeled_data["x_train"]}
+            x_test_input = {"x": labeled_data["x_test"]}
+
+            ensembler_train_input = model.predict({
+                model.data_key: x_train_input
+            })[model.output_key]
+
+            ensembler_test_input = model.predict({
+                model.data_key: x_test_input
+            })
+
+            ensembler_data = {
+                'x_train': ensembler_train_input,
+                'y_train': labeled_data['y_train'],
+                'x_test': ensembler_test_input,
+                'y_test': labeled_data['y_test']
+            }
+        else:
+            ensembler_data = None
+
+        ensemble = ensemble_modeler.build_model(ensembler_data)
+        model.ensemble = ensemble
+
+        # Generate metrics of all sub models (teacher, student, ensembler).
+        if labeled_data:
+            test_data = {"x": labeled_data["x_test"], "y": labeled_data["y_test"]}
+            try:
+                model.metrics = self.evaluate_model(test_data, model)
+            except Exception as e:
+                logging.error(
+                    (
+                        "Couldn't complete the submodel evaluation. "
+                        "Got the following error."
+                    )
+                )
+                logging.error(e)
+            else:
+                logging.info("Evaluated all sub models successfully.")
+
+        return model
+
+    def evaluate_model(self, test_data: dict, model: Model):
+        submodel_metrics = super().evaluate_model(test_data, model)
+        metrics = {'submodel_metrics': submodel_metrics}
+        # TODO: Compute overall model metrics
+        return metrics
+
---
+++ h1st/model/wrapper/multi_model.py
@@ -0,0 +1,89 @@
+import uuid
+import pandas as pd
+from h1st.model.predictive_model import PredictiveModel
+
+
+class MultiModel(PredictiveModel):
+    name = 'multi_model'
+    data_key = 'X'
+    output_key = 'predictions'
+
+    def __init__(self):
+        super().__init__()
+        self.models = {}
+        self.stats = {}
+
+    def predict(self, data: dict) -> dict:
+
+        X = data[self.data_key]
+        all_pred = []
+        for k, v in self.models.items():
+            pred_key = getattr(v, 'output_key', 'predictions')
+            pred = v.predict({v.data_key: X})[pred_key]
+
+            all_pred.append(
+                pd.Series(pred, name=f'{k}')
+            )
+
+        out = {self.output_key: pd.concat(all_pred, axis=1)}
+        return out
+
+    def persist(self, version=None):
+        models = self.models
+        model_info = self.model_info
+        for k, v in models.items():
+            model_version = v.persist()
+            model_info[k]['version'] = model_version
+            model_info[k]['model_class'] = v.__class__
+
+        self.stats['model_info'] = model_info
+        version = super().persist(version)
+        return version
+
+    def load(self, version=None):
+        super().load(version)
+
+        model_info = self.stats['model_info']
+        models = {}
+        for k, v in model_info.items():
+            models[k] = v['model_class']().load(v['version'])
+
+        self.models = models
+        return self
+
+    def _update_model_info(self):
+        model_info = self.stats.get('model_info')
+        if model_info is None:
+            model_info = {}
+
+        for k, v in self.models.items():
+            info = {
+                'model_name': k,
+                'model_type': v.name,
+                'metrics': v.metrics,
+                'stats': v.stats
+            }
+            if k in model_info.keys():
+                model_info[k].update(info)
+            else:
+                model_info[k] = info
+
+        self.stats['model_info'] = model_info
+
+    @property
+    def model_info(self):
+        self._update_model_info()
+        return self.stats['model_info']
+
+    def add_model(self, model, name=None):
+        if name is None:
+            name = model.name + '-' + uuid.uuid4().hex
+
+        self.models[name] = model
+        self._update_model_info()
+
+    def get_submodel_model_metrics(self):
+        metrics = {k: getattr(v, 'metrics', {})
+                   for k, v in self.models.items()}
+        return metrics
+
