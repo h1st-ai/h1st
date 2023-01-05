dbb16bf708bba874148e2200111be30eb0f73ada
Tuan Pham <phamhoangtuan.it@gmail.com>
2023-01-05 16:19:33 +0700

    fix: change TimeSeriesOracleModel to TimeSeriesOracle

--- h1st/model/oracle/__init__.py
+++ h1st/model/oracle/__init__.py
@@ -1,4 +1,4 @@
 from h1st.model.oracle.oracle_modeler import OracleModeler
 from h1st.model.oracle.oracle import Oracle
 from h1st.model.oracle.ts_oracle_modeler import TimeseriesOracleModeler
-from h1st.model.oracle.ts_oracle_model import TimeSeriesOracleModel
+from h1st.model.oracle.ts_oracle_model import TimeSeriesOracle
