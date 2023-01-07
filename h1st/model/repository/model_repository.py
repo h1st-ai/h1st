b0d1f725c8d7bfdb9b43dbe24b6619c3bb1a0d6f
TrellixVulnTeam <charles.mcfarland@trellix.com>
2023-01-07 12:40:18 +0000

    Adding tarfile member sanitization to extractall()

--- h1st/model/repository/model_repository.py
+++ h1st/model/repository/model_repository.py
@@ -593,4 +593,23 @@ def _tar_extract(source, target):
     Helper function to extract a tar archive
     """
     with tarfile.open(source) as tf:
-        tf.extractall(target)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner) 
+            
+        
+        safe_extract(tf, target)
